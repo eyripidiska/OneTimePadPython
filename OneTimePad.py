e34240a5a1e8fc1fa4209c81b2eb40e2f1b9009c
Evripidis Katsianos <euripidiskatsianos@gmail.com>
2023-12-16 03:11:21 +0200

    final

--- OneTimePad.py
+++ OneTimePad.py
@@ -1,17 +1,6 @@
 import random
 import binascii
 
-
-def encrypt(message, one_time_pad):
-    # Computes the subset of the one time pad to be used for encryption
-    subset_one_time_pad = one_time_pad[:len(message)]
-    
-    # XORs (bitwise exclusive OR) each byte of the message with the corresponding byte of the one-time pad
-    encrypted_bytes = bytes([(m_byte ^ otp_byte) for m_byte, otp_byte in zip(message, subset_one_time_pad)])
-    
-    return encrypted_bytes
-
-
 def get_key(message, cipher, length):
    
     # XORs (bitwise exclusive OR) each byte of the message with the corresponding byte of cipher to get the key
@@ -48,7 +37,7 @@ def decrypt(cipher, key):
 
     ascii_string += '*' * remain_length
 
-    print("Unknown Message:", ascii_string)
+    print("Unknown Message Value:", ascii_string)
     print("Binary Representation Unknown Message: ")
     for b in message_to_bytes:
         binary_representation = bin(b)[2:].zfill(8)
@@ -69,42 +58,72 @@ def print_bytes(bytes_to_print):
         print(binary_representation, end=" ")
     print("\n--------------------------------------------------------------------------------------------------")
 
+def bits_to_bytes(bits):
+    # Ensure the length of bits is a multiple of 8 by adding leading zeros if needed
+    while len(bits) % 8 != 0:
+        bits = '0' + bits
+
+    # Convert the binary string to an integer
+    decimal_value = int(bits, 2)
+
+    # Calculate the number of bytes required
+    num_bytes = (len(bits) + 7) // 8
+
+    # Convert the integer to bytes
+    byte_representation = decimal_value.to_bytes(num_bytes, byteorder='big')
+
+    return byte_representation
 
 
 
-#print("One-Time Pad Encryption")
-message1_case_smaller = "Small"
-message1_case_equal = "Equals"
-message1_case_bigger = "Big Message"
-
+# message1_case_smaller = "Small"
+# message1_case_equal = "Equals"
+# message1_case_bigger = "Big Message"
 # key = 0100111000101010100100101101111001000001000001011001011000011010100011000001100100100011
-# m1 =  0100001001101001011001110010000001001101011001010111001101110011011000010110011101100101
-# c1 =  0000110001000011111101011111111000001100011000001110010101101001111011010111111001000110
-# c2 =  000111010100111111110001101011000010010001110001
-message1 = message1_case_bigger
-message2 = "Secret"
+# m1_case_smaller =  "0101001101101101011000010110110001101100"
+# c1_case_smaller =  "0001110101000111111100111011001000101101"
+# m1_case_equal =  "010001010111000101110101011000010110110001110011"
+# c1_case_equal =  "000010110101101111100111101111110010110101110110"
+# m1_case_bigger =  "0100001001101001011001110010000001001101011001010111001101110011011000010110011101100101"
+# c1_case_bigger =  "0000110001000011111101011111111000001100011000001110010101101001111011010111111001000110"
 
-# Generate a one-time pad key by the longest message
-one_time_pad_bytes = bytes([random.randint(0, 255) for _ in range(max(len(message1), len(message2)))])  
+dict_case_smaller = {
+  "message": "Small",
+  "cipher": "0001110101000111111100111011001000101101"
+}
 
-print("Message 1:", message1)
+dict_case_equal = {
+  "message": "Equals",
+  "cipher": "000010110101101111100111101111110010110101110110"
+}
+
+dict_case_bigger = {
+  "message": "Big Message",
+  "cipher": "0000110001000011111101011111111000001100011000001110010101101001111011010111111001000110"
+}
+
+selected_dict = dict_case_bigger
+
+m1 = selected_dict["message"]
+cipher1 =  selected_dict["cipher"]
+cipher2 =  "000111010100111111110001101011000010010001110001"
+
+c1 = bits_to_bytes(cipher1)
+c2 = bits_to_bytes(cipher2)
+
+print("Message 1 Value:",m1)
 print("Binary Representation Message 1: ")
-message1_to_bytes = bytes(message1, 'ascii') 
+message1_to_bytes = bytes(m1, 'ascii') 
 print_bytes(message1_to_bytes)
 
-message2_to_bytes = bytes(message2, 'ascii')
-
-# Encrypt the messages using the one-time pad
 print("Binary Representation Cipher 1: ")
-c1 = encrypt(message1_to_bytes, one_time_pad_bytes)
 print_bytes(c1)
 
 print("Binary Representation Cipher 2: ")
-c2 = encrypt(message2_to_bytes, one_time_pad_bytes)
 print_bytes(c2)
 
 print("Binary Representation Key: ")
-key = get_key(message1_to_bytes, c1, max(len(message1), len(message2)))
+key = get_key(message1_to_bytes, c1, max(len(c1), len(c2)))
 
 message2 = decrypt(c2, key)
 
